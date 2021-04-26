26a7451953839dafd3f552c033cd51c60f71943b
Nicole Chung <nicole.chung@circleci.com>
2021-04-25 22:05:24 -0400

    initial fake test

---
+++ src/form.test.ts
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+
+describe('Form', () => {
+    it('renders and fetches the data, displaying it in the form', () => {
+        expect(true).toEqual(true)
+    })
+})
