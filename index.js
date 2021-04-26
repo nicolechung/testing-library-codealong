5deb1d4afc6d6a8e3b82ca77aa0aa2b50733b8bd
Nicole Chung <nicole.chung@circleci.com>
2021-04-25 21:39:05 -0400

    changes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Form } from "./src/form";
 
-const domContainer = document.querySelector("#like_button_container");
+const domContainer = document.querySelector("#form");
 ReactDOM.render(<Form />, domContainer);
